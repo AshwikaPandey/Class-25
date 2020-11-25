c8b91313222505f05f9085dd3e016ddbe7ef17f6
AshwikaPandey <74997637+AshwikaPandey@users.noreply.github.com>
2020-11-24 21:01:37 -0500

    Add files via upload

---
+++ Trash.js
@@ -0,0 +1,32 @@
+class Trash {
+constructor(x,y){
+var options={
+//isStatic:true,
+restitution:0.4,
+friction:0.5,
+density:1.2
+}
+this.body= Bodies.circle(x,y,20,options)
+this.width=20;
+this.height=20;
+this.Image= loadImage("paper.png")
+World.add(world ,this.body)
+
+}
+display(){
+  var pos= this.body.position
+  var angle=this.body.angle
+push()
+translate(pos.x,pos.y)
+rotate(angle)
+//ellipseMode(RADIUS)
+//ellipse(0,0,this.width,this.height)
+ImageMode(CENTER)
+Image(this.Image,0,0,this.width,this.height)
+
+fill(440);
+
+pop()
+
+}
+}
